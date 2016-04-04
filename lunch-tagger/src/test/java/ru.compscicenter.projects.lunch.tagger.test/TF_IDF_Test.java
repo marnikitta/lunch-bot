a4c92983b01efd4437ef26de4f5df96722eb47e6
Vikulov Evgenii <jenyav94@gmail.com>
2016-04-04 13:41:18 +0300

    added TF_IDF.java

---
+++ lunch-tagger/src/test/java/ru.compscicenter.projects.lunch.tagger.test/TF_IDF_Test.java
@@ -0,0 +1,27 @@
+package ru.compscicenter.projects.lunch.tagger.test;
+
+import org.testng.annotations.Test;
+import ru.compscicenter.projects.lunch.tagger.TF_IDF;
+
+public class TF_IDF_Test {
+
+    @Test
+    public void testTfIdfMatrix() {
+        //Wiki example
+        final String[] docs = new String[]{"this is a a sample", "this is another another example example example"};
+        final double[] expectedArray = {0.04300, 0.04300, 0, 0, 0.13632, 0.20448};
+        final double eps = 0.0001;
+
+        TF_IDF tf_idf = new TF_IDF(docs);
+        double[][] tf_idf_matrix = tf_idf.getTF_IDFMatrix();
+        double[] doc2Vector = tf_idf_matrix[1];
+        int doc2Length = tf_idf.getWordVectorLength();
+
+        for (int valueInd = 0; valueInd < doc2Length; ++valueInd) {
+            double actual = doc2Vector[valueInd];
+            double expected = expectedArray[valueInd];
+            assert (Math.abs(actual - expected) < eps);
+        }
+    }
+
+}
