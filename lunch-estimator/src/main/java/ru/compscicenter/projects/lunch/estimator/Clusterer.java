bf77acdf52e3bc6b644c9e5510a9db6c4dbf5662
Vikulov Evgenii <jenyav94@gmail.com>
2016-04-27 16:11:44 +0300

    change argument from Collection to List

--- lunch-estimator/src/main/java/ru/compscicenter/projects/lunch/estimator/Clusterer.java
+++ lunch-estimator/src/main/java/ru/compscicenter/projects/lunch/estimator/Clusterer.java
@@ -2,7 +2,7 @@ package ru.compscicenter.projects.lunch.estimator;
 
 import ru.compscicenter.projects.lunch.model.MenuItem;
 
-import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 public interface Clusterer {
@@ -12,5 +12,5 @@ public interface Clusterer {
      * @param collection коллекция блюд
      * @return блюдо - номер кластера
      */
-    public Map<MenuItem, Integer> doCluster(Collection<? extends MenuItem> collection);
+    public Map<MenuItem, Integer> doCluster(List<? extends MenuItem> sample);
 }
